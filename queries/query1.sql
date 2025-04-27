a1f96a6ecbd0d585c547a296e5cd9dcd7b949a30
chris-sooseok <sooseokkim99@gmail.com>
2025-04-27 01:32:04 -0400

    query 1 and 2

--- queries/query1.sql
+++ queries/query1.sql
@@ -1,2 +1,6 @@
 -- Insert Property
-INSERT INTO property VALUES (99, 'MAIN ST', 'Cheektowaga', '14001', 250000, 1800, 2005, 'house');
+INSERT INTO address (number, street, city, zip_code, state)
+VALUES (99, 'MAIN ST', 'Cheektowaga', '14001', 'NY');
+
+INSERT INTO property
+VALUES (99, 'MAIN ST', 'Cheektowaga', '14001', 250000, 1800, 2005, 'single house');
--- queries/query2.sql
+++ queries/query2.sql
@@ -1,2 +1,11 @@
--- Property Delete
-DELETE FROM property WHERE number = 99 AND street = 'MAIN ST' AND city = 'Cheektowaga' AND zip_code = '14001';
+DELETE FROM property
+WHERE number = 99
+  AND street = 'MAIN ST'
+  AND city = 'Cheektowaga'
+  AND zip_code = '14001';
+
+DELETE FROM address
+WHERE number = 99
+  AND street = 'MAIN ST'
+  AND city = 'Cheektowaga'
+  AND zip_code = '14001';
